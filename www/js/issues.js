84e03e49cfb236a5b9670fa71f7cf89c087d2b2f
noemi <noemi.lopez@heig-vd.ch>
2017-03-17 10:29:34 +0100

    carte fonctionnel avec mapbox

--- www/js/issues.js
+++ www/js/issues.js
@@ -65,3 +65,55 @@ angular.module('citizen-engagement')
             })
         };
         });
+        angular.module('citizen-engagement').controller('NewIssueCtrl', function(geolocation, $log) {
+          var newIssueCtrl = this;
+
+          geolocation.getLocation().then(function(data){
+            newIssueCtrl.latitude = data.coords.latitude;
+            newIssueCtrl.longitude = data.coords.longitude;
+          }).catch(function(err) {
+            $log.error('Could not get location because: ' + err.message);
+          });
+        });
+
+        angular.module('citizen-engagement').controller('MapCtrl', function($scope, mapboxSecret) {
+          var mapCtrl = this;
+          var record = {
+            title: 'Lorem ipsum'
+          };
+
+          var mapboxMapId = 'mapbox.satellite';
+          var mapboxAccessToken = mapboxSecret;
+
+          var mapboxTileLayerUrl = 'http://api.tiles.mapbox.com/v4/' + mapboxMapId;
+          mapboxTileLayerUrl = mapboxTileLayerUrl + '/{z}/{x}/{y}.png';
+          mapboxTileLayerUrl = mapboxTileLayerUrl + '?access_token=' + mapboxAccessToken;
+
+          $scope.$on('leafletDirectiveMap.dragend', function(event, map){
+            console.log('Map was dragged');
+          });
+          $scope.$on('leafletDirectiveMarker.click', function(event, marker) {
+            var coords = marker.model.lng + '/' + marker.model.lat;
+            console.log('Marker at ' + coords + ' was clicked');
+          });
+
+          mapCtrl.defaults = {
+            ti leLayer: mapboxTileLayerUrl
+          };
+          mapCtrl.markers=[];
+          mapCtrl.center = {
+            lat: 51.48,
+            lng: 0,
+            zoom: 14
+          };
+          mapCtrl.markers.push({
+            lat: 51.48,
+            lng: 0,
+            message: '<div ng-include="\'templates/message.html\'"/"/>',
+            getMessageScope: function() {
+              var scope = $scope.$new();
+              scope.record = record;
+              return scope;
+            }
+          });
+        });
