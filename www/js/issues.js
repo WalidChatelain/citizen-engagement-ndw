d4b4082fc83b97135d19e07f7f79583992c598ab
Walid Chatelain <walid.chatelain@heig-vd.ch>
2017-03-20 10:25:08 +0100

    small corrections

--- www/js/issues.js
+++ www/js/issues.js
@@ -43,30 +43,6 @@ angular.module('citizen-engagement')
 
 })
 
-/*.controller("newIssueCtrl", function($scope, $http, apiUrl, $stateParams, GeolocService){
-$scope.loadIssueTypes = function () {
-$scope.issue = {};
-$http({
-method: 'GET',
-url: apiUrl + '/issueTypes/'
-}).success(function (issueTypes) {
-$scope.issueTypes = issueTypes;
-GeolocService.locateUser().then(function (coords) {
-console.log(coords);
-$scope.issue.lng = coords.longitude;
-$scope.issue.lat = coords.latitude;
-});
-});
-};
-$scope.loadIssueTypes();
-$scope.submit = function () {
-$scope.issue.imgUrl = "http://cliparts.co/cliparts/8c6/oAK/8c6oAKjri.png";
-console.log($scope.issue);
-$http.post(apiUrl + '/issues/', $scope.issue).success(function(res) {
-console.log(res);
-})
-};
-});*/
 
 angular.module('citizen-engagement').controller('newIssueCtrl', function(geolocation, $log, $scope, $http, apiUrl, $ionicPopup, CameraService) {
   var newIssueCtrl = this;
